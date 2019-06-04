304e1fdabfc0f784b07adfef36f4b5003b1db0e4
Qing-Cheng Li <qc.linux@gmail.com>
2019-06-04 23:04:00 +0200

    add service worker

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'static-cache-v1';
+
+const FILES_TO_CACHE = [
+    './index.html',
+    './0.index.js',
+    './1.index.js',
+    './ba809633fed94322bc52.module.wasm',
+    './index.js',
+    './styles.css'
+];
+
+self.addEventListener('install', (evt) => {
+    console.log('[Service Worker] install');
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('[Service Worker] Pre-cache');
+            return cache.addAll(FILES_TO_CACHE);
+        });
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+    console.log('[Service Worker] Activate');
+    evt.waitUntil(
+        caches.keys().then((keyList) => {
+            if (key !== CACHE_NAME) {
+                console.log('[Service Worker] Removing old cache', key);
+                return caches.delete(key);
+            }
+        });
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+    console.log('[Service Worker] Fetch', evt.request.url);
+
+    evt.respondWith(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.match(evt.request).then((response) => {
+                return response || fetch(evt.request);
+            });
+        });
+    );
+});
+
